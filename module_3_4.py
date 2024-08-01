fc2e9c800156db7b91062266653ccb1932f80873
DjonKrd <dj2393@mail.ru>
2024-08-01 10:47:22 +0300

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -1,10 +1,8 @@
 def single_root_words(root_word, *other_words):
     same_words = []
-    root_word = root_word.lower()
     for i in range(len(other_words)):
-        i = other_words[i].lower()
-        if root_word in i or i in root_word:
-            same_words.append(i)
+        if root_word.lower() in other_words[i].lower() or other_words[i].lower() in root_word.lower():
+            same_words.append(other_words[i])
     return same_words
 
 
